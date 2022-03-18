1a2d1bad9054e7ec3e00053a2f07c26000bc77dd
llFireHawkll <sparsh.dtt@gmail.com>
2022-03-19 00:25:54 +0530

    pushing exceptions logic

--- neuraml/exceptions/exceptions.py
+++ neuraml/exceptions/exceptions.py
@@ -1,22 +1,34 @@
 __all__ = [
-    "NoneException",
-    "NotInListException",
-    "EmptyDataFrameException",
-    "VariableInitializationException",
+    "NoneError",
+    "InstanceNotCalledError",
+    "NotInListError",
+    "EmptyListError",
+    "EmptyDataFrameError",
+    "VariableInitializationError",
 ]
 
 
-class NoneException(Exception):
+class NoneError(Exception):
     def __init__(self, msg=None):
         if msg is None:
-            self.msg = "None Value Passed!"
+            self.msg = "None Value Assigned To Variable!"
         else:
             self.msg = msg
 
         super().__init__(self.msg)
 
 
-class NotInListException(Exception):
+class InstanceNotCalledError(Exception):
+    def __init__(self, msg=None):
+        if msg is None:
+            self.msg = "Instance not called, Please call the instance() method / __call__ operator!"
+        else:
+            self.msg = msg
+
+        super().__init__(self.msg)
+
+
+class NotInListError(Exception):
     def __init__(self, msg=None):
         if msg is None:
             self.msg = "Variable Not Present In List!"
@@ -26,7 +38,17 @@ class NotInListException(Exception):
         super().__init__(self.msg)
 
 
-class EmptyDataFrameException(Exception):
+class EmptyListError(Exception):
+    def __init__(self, msg=None):
+        if msg is None:
+            self.msg = "Please Provide Valid List Empty List Found!"
+        else:
+            self.msg = msg
+
+        super().__init__(self.msg)
+
+
+class EmptyDataFrameError(Exception):
     def __init__(self, msg=None):
         if msg is None:
             self.msg = "Please Provide Valid DataFrame Empty DataFrame Found!"
@@ -36,7 +58,7 @@ class EmptyDataFrameException(Exception):
         super().__init__(self.msg)
 
 
-class VariableInitializationException(Exception):
+class VariableInitializationError(Exception):
     def __init__(self, msg=None):
         if msg is None:
             self.msg = "Variable Not Initialized!"
