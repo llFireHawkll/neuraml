e9ed33ffeb7eb3bbab526d96c4aa8e6a884fb27c
llFireHawkll <sparsh.dtt@gmail.com>
2022-03-19 19:47:05 +0530

    pushing base class for engine dataloader

---
+++ neuraml/core/data/engine.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+import pandas as pd
+from neuraml.core.data.featureset import ClsDataFeatureSet
+from neuraml.core.data.indexing import ClsDataIndexing
+from neuraml.core.data.preprocessing import ClsDataPreProcessing
+from neuraml.exceptions.exceptions import (
+    EmptyDataFrameError,
+    InstanceNotCalledError,
+    NoneError,
+)
+
+__all__ = [
+    "ClsPandasDataLoader",
+]
+
+
+class ClsBaseDataLoader(ABC):
+    def __init__(self):
+        """_summary_"""
+        pass
+
+    @abstractmethod
+    def _read_data_s3(self):
+        """_summary_"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def _write_data_s3(self):
+        """_summary_"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def _read_data_local(self, path, file_type, **kwargs):
+        """_summary_
+
+        Args:
+            path (_type_): _description_
+            file_type (_type_): _description_
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def _write_data_local(self, path, file_type, **kwargs):
+        """_summary_
+
+        Args:
+            path (_type_): _description_
+            file_type (_type_): _description_
+        """
+        raise NotImplementedError
