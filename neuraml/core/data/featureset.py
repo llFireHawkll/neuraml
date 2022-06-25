9e1aeed1f7b813661428707dcd8513bf66b56cc4
llFireHawkll <sparsh.dtt@gmail.com>
2022-06-25 19:56:19 +0530

    pushing updated featureset class

--- neuraml/core/data/featureset.py
+++ neuraml/core/data/featureset.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Optional, Union
 
 from neuraml.exceptions.exceptions import EmptyListError, InstanceNotCalledError
+from pandas.api.types import is_object_dtype
 from pydantic import BaseModel, validator
 
 __all__ = ["ClsDataFeatureSet"]
@@ -17,6 +18,7 @@ class FeatureSetColumns(BaseModel):
 
     class Config:
         extra = "allow"
+        arbitrary_types_allowed = True
 
     @validator("regression")
     def validate_regression(cls, value):
@@ -26,8 +28,20 @@ class FeatureSetColumns(BaseModel):
 
 
 class ClsDataFeatureSet(FeatureSetColumns):
+    """ClsDataFeatureSet class is responsible for maintaining
+    the underlying configuration for our data model. This class
+    has method to idenify variables as numerical, categorical and object types
+
+    Args:
+        FeatureSetColumns (_type_): _description_
+    """
+
     def __init__(self, **kwargs) -> None:
+        """Initializing the FeatureSetColumns pydantic basemodel with required parameters"""
         FeatureSetColumns.__init__(self, **kwargs)
+
+        # Check and set the id variable
+        self._check_and_set_id_variable()
 
         # Setting up internal attributes
         self.state_flag = False  # For storing the state of instance
@@ -35,16 +49,27 @@ class ClsDataFeatureSet(FeatureSetColumns):
         self.numerical_cols = list()  # For storing numerical columns
         self.categorical_cols = list()  # For storing categorical columns
 
+    def _check_and_set_id_variable(self):
+        """This method checks and set the index of the featureset
+        This "id_" will be used as primary key
+        """
+        if self.id_ is None:
+            self.id_ = "index"
+
     def _get_features_segmentation(self):
-        """_summary_"""
+        """This method checks and classifies variables into
+        numerical, categorical and object dtypes.
+        """
         # Step-1 Iterate each variable from dtypes dict
         for variable in self.dtypes.keys():
-            if variable == self._id:
+            if variable == self.id_:
                 pass
             elif variable == self.time:
                 pass
+            elif variable == self.target:
+                pass
             else:
-                if isinstance(self.dtypes[variable], object):
+                if is_object_dtype(self.dtypes[variable]):
                     self.objects_cols.append(variable)
                 else:
                     self.numerical_cols.append(variable)
@@ -53,11 +78,13 @@ class ClsDataFeatureSet(FeatureSetColumns):
         # and dump the variables which have atleast 10 unique values
         # into categorical_cols list
         for variable in self.objects_cols:
-            if self.dtypes[variable] <= 10:
+            if self.nuniques[variable] <= 10:
                 self.categorical_cols.append(variable)
 
     def __call__(self):
-        """ """
+        """This method is where the instance internal methods are called and all the
+        magic happens here.
+        """
         # Step-1 Setting state_flag of instance to True
         self.state_flag = True
 
@@ -65,30 +92,29 @@ class ClsDataFeatureSet(FeatureSetColumns):
         self._get_features_segmentation()
 
     def __repr__(self):
+        """This method is used to get a summary of the featureset instance"""
         # Step-1 Check state_flag status
         if self.state_flag:
-            return (
-                "Feature Set:"
-                + "\nTarget Variable: "
-                + str(self.target)
-                + "\nRegression Variables: "
-                + ", ".join(self.regression)
-                + "\nSegmentation Variables: "
-                + ", ".join(self.segmentation)
-                + "\nRegression Variables Count: "
-                + str(len(self.regression))
-                + "\nSegmentation Variables Count: "
-                + str(len(self.segmentation))
-                + "\nNumerical Variables Count: "
-                + str(len(self.numerical_cols))
-                + "\nCategorical Variables Count: "
-                + str(len(self.categorical_cols))
-                + "\nObject Variables Count: "
-                + str(len(self.objects_cols))
-            )
+            output_string = f"""Feature Set Configuration:
+            Target Variable: "{str(self.target)}"
+            Regression Variables: ["{'", '.join(self.regression)}"]
+            Segmentation Variables: ["{'", '.join(self.segmentation)}"]
+            Regression Variables Count: {str(len(self.regression))}
+            Segmentation Variables Count: {str(len(self.segmentation))}
+            Numerical Variables Count: {str(len(self.numerical_cols))}
+            Categorical Variables Count: {str(len(self.categorical_cols))}
+            Object Variables Count: {str(len(self.objects_cols))}"""
+
+            return output_string
         else:
             # Step-2 Else raise error
             raise InstanceNotCalledError()
 
     def __str__(self):
+        """This method is used to get a summary of the featureset instance when print()
+        is called. Internally calls __repr__() special method.
+
+        Returns:
+            __repr__: This method is used to get a summary of the featureset instance
+        """
         return self.__repr__()
